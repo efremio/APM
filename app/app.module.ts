dfd053c95c5b74f529b28e33934f4b7b811468af
eagnilleri <eagnilleri@guidewire.com>
2017-01-17 17:41:46 +0000

    http request

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
 import { ProductListConponent } from './products/product-list.component';
+import { ProductFilterPipe } from './products/product-filter.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule //we register the http service in the root module
   ],
   declarations: [
     AppComponent,
-    ProductListConponent ],
+    ProductListConponent,
+    ProductFilterPipe,
+    StarComponent
+    ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,52 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: 'app/products/product-list.component.html',
-    styleUrls: ['app/products/product-list.component.css']
+    moduleId: module.id,    //if we use module.id we can use relative paths!
+    templateUrl: 'product-list.component.html', //relative path
+    styleUrls: ['product-list.component.css']
 })
 
-export class ProductListConponent {
+export class ProductListConponent implements OnInit{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter:string = 'cart';
-    products: IProduct[] = [
-    {
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2016",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    },
-    {
-        "productId": 8,
-        "productName": "Saw",
-        "productCode": "TBX-0022",
-        "releaseDate": "May 15, 2016",
-        "description": "15-inch steel blade hand saw",
-        "price": 11.55,
-        "starRating": 3.7,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
-    },
-    {
-        "productId": 10,
-        "productName": "Video Game Controller",
-        "productCode": "GMG-0042",
-        "releaseDate": "October 15, 2015",
-        "description": "Standard two-button video game controller",
-        "price": 35.95,
-        "starRating": 4.6,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
-    }
-];
+    listFilter:string;
+    products: IProduct[];
+    errorMessage: string;
 
-toggleImage(): void {
-    this.showImage = !this.showImage;
-}
+    constructor(private _productService: ProductService) { } //automatically assigns the parameter to the private var
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+
+    ngOnInit() {
+        this._productService.getProducts() //observables are lazy. the don't start to emit values until someone calls subscribe
+            .subscribe(products => this.products = products, //the action to take every time an item is emitted (in this case there is only one item)
+            error => this.errorMessage = <any>error); //if the observable fails
+            //there is another optional function that is called when the observer has finished emitting items. not really used for one-item returns
+ }
+
+    onRatingClicked(message: string) { //the parameter is the payload
+        this.pageTitle = 'Product List: ' + message;
+    }
+
 }
