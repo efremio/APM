13d59a51b48ce6c8526d9f18692b7870d3479d2f
eagnilleri <eagnilleri@guidewire.com>
2017-01-24 17:38:30 +0000

    product detail view

--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,24 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     templateUrl: 'app/products/product-detail.component.html'
 })
 
-export class ProductDetailComponent implements OnInit{
+export class ProductDetailComponent implements OnInit, OnDestroy {
     pageTitle: string = "Product detail";
     product: IProduct;
+    errorMessage: string;
+    private sub: Subscription;
 
     constructor(private _route: ActivatedRoute,
-                private _router: Router) {
+                private _router: Router,
+                private _productService: ProductService) {
         
     }
 
     ngOnInit(): void {
-        let id = +this._route.snapshot.params['id']; //+ is a js shortcut to convert strings to numbers
-        this.pageTitle += `: ${id}`;
+        this.sub = this._route.params.subscribe(
+            params => {
+                let id = +params['id'];
+                this.getProduct(id);
+        });
     }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+
+    getProduct(id: number) {
+        this._productService.getProduct(id).subscribe(
+            product => this.product = product,
+            error => this.errorMessage = <any>error);
+    }
+
 
 /* We could use the routerLink directive,
 but here we use the code to navigate back.
